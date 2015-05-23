dbce40dc11c33d5d35c3106648a90094093f6662
Blake Lowe <b.f.lowe@csuohio.edu>
2015-05-23 09:34:12 -0400

    add lic info

--- abilityscores5.py
+++ abilityscores5.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python
 # ability score generator for DnD 5th ed.
+#
+# Copyright (C) 2015 - Blake Lowe
+#
+# This file is part of rpg-utils.
+#
+# rpg-utils is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# rpg-utils is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with rpg-utils. If not, see <http://www.gnu.org/licenses/>.
+#
 import random
 
 def get_score():
